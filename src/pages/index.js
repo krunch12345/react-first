0b5954dd68347db80ac96af47e454dada03fe7bf
Konstantin Chukreev <krunch12345@gmail.com>
2022-04-04 16:39:59 +0300

    l8

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import { Messenger } from './Messenger/Messenger'
 import { Home } from './Home/Home'
 import { Profile } from './Profile/Profile'
+import { GistsList } from './GistsList/GistsList'
 
 export {
     Messenger,
     Home,
-    Profile
+    Profile,
+    GistsList,
 }
---
+++ src/store/gists/reducer.js
@@ -0,0 +1,32 @@
+import { STATUSES } from '../constants/statuses'
+import { GET_GISTS_FAILURE, GET_GISTS_REQUEST, GET_GISTS_SUCCESS } from './actions'
+
+const initialState = {
+    gists: [],
+    request: STATUSES.IDLE,
+    error: null,
+}
+
+export const gistsReducer = (state = initialState, action) => {
+    switch (action.type) {
+    case GET_GISTS_REQUEST:
+        return {
+            ...state,
+            request: STATUSES.REQUEST,
+        }
+    case GET_GISTS_SUCCESS:
+        return {
+            ...state,
+            articles: action.payload,
+            request: STATUSES.SUCCESS,
+        }
+    case GET_GISTS_FAILURE:
+        return {
+            ...state,
+            request: STATUSES.FAILURE,
+            error: action.payload,
+        }
+    default:
+        return state
+    }
+}
